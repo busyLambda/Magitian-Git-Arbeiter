67bbb8dac771836f6fb16bc013d468c6a369f01e
Susa Milán <mailtosnek@protonmail.com>
2023-02-09 13:01:58 +0100

    Web server and services

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+mod services;
+
+use actix_web::{web::scope, App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(scope("/api")))
+        .bind(("0.0.0.0", 8984))?
+        .run()
+        .await
 }
