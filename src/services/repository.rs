56d1c316329ea12e549c7948c10a601e85a821e0
Susa Milán <mailtosnek@protonmail.com>
2023-02-10 09:47:25 +0100

    Create new repo in the api

--- src/services/repository.rs
+++ src/services/repository.rs
@@ -1,13 +1,18 @@
-use actix_web::{get, post, Responder, web::Query, delete};
+use actix_web::{get, Responder, web::Query, delete};
+use magitian_models::git::RawRepository;
+use crate::extras::repository::create_repo;
+
 use super::int_repr::queries::NewRepoQ;
 
 #[get("/new")]
 pub async fn new_repository(repo_form: Query<NewRepoQ>) -> impl Responder {
-    let repo_form = repo_form.into_inner();
+    let rf = repo_form.into_inner();
 
-    format!("User dir: {}, Repo name: {}", repo_form.user_dir, repo_form.repo_name)
+    create_repo(&rf.user_dir, &rf.repo_name).unwrap();
+    format!("User dir: {}, Repo name: {}", rf.user_dir, rf.repo_name)
 }
-#[delete("/new")]
+
+#[delete("/delete")]
 pub async fn delete_repository(repo_form: Query<NewRepoQ>) -> impl Responder {
     let repo_form = repo_form.into_inner();
 
