7a00ebb587890fa8a26db4384de7135a4ec83a1b
Susa Milán <mailtosnek@protonmail.com>
2023-02-10 11:06:02 +0100

    Added some basic error handling

--- src/services/repository.rs
+++ src/services/repository.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, Responder, web::Query, delete};
+use actix_web::{get, Responder, web::Query, delete, HttpResponse};
 use magitian_models::git::RawRepository;
 use crate::extras::repository::create_repo;
 
@@ -8,8 +8,10 @@ use super::int_repr::queries::NewRepoQ;
 pub async fn new_repository(repo_form: Query<NewRepoQ>) -> impl Responder {
     let rf = repo_form.into_inner();
 
-    create_repo(&rf.user_dir, &rf.repo_name).unwrap();
-    format!("User dir: {}, Repo name: {}", rf.user_dir, rf.repo_name)
+    match create_repo(&rf.user_dir, &rf.repo_name) {
+        Ok(_) => HttpResponse::Ok().body(format!("User dir: {}, Repo name: {}", rf.user_dir, rf.repo_name)),
+        Err(e) => HttpResponse::InternalServerError().body(format!("Cannot create repository: {}", e)),
+    }
 }
 
 #[delete("/delete")]
