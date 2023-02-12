98c40ff5b03505c43ad95900788a3432fe71adc9
Susa Milán <mailtosnek@protonmail.com>
2023-02-12 08:19:45 +0100

    Cleanup + fmt

--- src/services/object.rs
+++ src/services/object.rs
@@ -1,7 +1,7 @@
 use actix_web::{get, web::Path, HttpResponse, Responder};
 use git2::{Oid, Repository};
 
-use crate::extras::object::{TreeIterator, Component};
+use crate::extras::object::{Component, TreeIterator};
 
 #[get("/tree/{path}")]
 pub async fn tree() -> impl Responder {
@@ -29,29 +29,4 @@ pub async fn blob(path: Path<(String, String, String)>) -> impl Responder {
     let contents = tri.filter_map(|r| r.ok()).flatten().last();
 
     HttpResponse::Ok().body(contents.unwrap())
-}
-
-fn get_blob_contents(repo_path: String, oid: Oid) -> String {
-    let repo = Repository::open(repo_path).unwrap();
-    let b = repo.find_blob(oid).unwrap();
-    let contents = std::str::from_utf8(b.content()).unwrap();
-    contents.to_string()
-}
-
-fn find_blob_via_path(repo: Repository, path: &String, branch_name: &str) -> Option<Oid> {
-    let branch = repo
-        .find_branch(branch_name, git2::BranchType::Local)
-        .unwrap();
-    let target = branch.get().target().unwrap();
-    let commit = repo.find_commit(target).unwrap();
-    let t = commit.tree().unwrap();
-
-    for entry in t.iter() {
-        println!("entry: {:?}", entry.name());
-        if entry.name().unwrap() == path {
-            return Some(entry.id());
-        }
-    }
-
-    None
 }
