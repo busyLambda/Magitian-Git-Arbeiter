b77fd06443b3d9faac434bcbdfd269e65cf020a9
Susa Milán <mailtosnek@protonmail.com>
2023-02-14 19:13:59 +0100

    Remove unwrap

--- src/services/object.rs
+++ src/services/object.rs
@@ -31,14 +31,35 @@ pub async fn blob(path: Path<(String, String, String)>) -> impl Responder {
     debug!("Getting blob at: {}", &path.2);
 
     let p = path.2.to_owned();
-    let repo = Repository::open(format!("git_test/{}/{}/", path.0, path.1)).unwrap();
-    let branch = repo.find_branch("master", git2::BranchType::Local).unwrap();
-    let commit = repo.find_commit(branch.get().target().unwrap()).unwrap();
-    let t = commit.tree().unwrap();
+    let repo = match Repository::open(format!("git_test/{}/{}/", path.0, path.1)) {
+        Ok(r) => r,
+        Err(e) => return HttpResponse::NotFound().body(format!("Cannot find repository: {}", e)),
+    };
+    let branch = match repo.find_branch("master", git2::BranchType::Local) {
+        Ok(b) => b,
+        Err(e) => return HttpResponse::NotFound().body(format!("Cannot find branch: {}", e)),
+    };
+    let commit = match repo.find_commit(match branch.get().target() {
+        Some(b) => b,
+        None => return HttpResponse::NotFound().body(format!("Cannot get commit from branch")),
+    }) {
+        Ok(c) => c,
+        Err(e) => return HttpResponse::NotFound().body(format!("Cannot find commit: {}", e)),
+    };
+    let t = match commit.tree() {
+        Ok(t) => t,
+        Err(te) => {
+            return HttpResponse::InternalServerError()
+                .body(format!("Failed to get tree from commit: {}", te))
+        }
+    };
     let components = Component::from_string(p);
 
     let tri = TreeIterator::new(&repo, t, components);
-    let resp = match tri.filter_map(|r| r.ok()).flatten().last().unwrap() {
+    let resp = match match tri.filter_map(|r| r.ok()).flatten().last() {
+        Some(resp) => resp,
+        None => return HttpResponse::InternalServerError().body(format!("Failed to walk tree")),
+    } {
         BoTo::Blob(blob) => HttpResponse::Ok().json(blob),
         _ => HttpResponse::InternalServerError()
             .body("Found non blob item on the blob api endpoint, what?"),
