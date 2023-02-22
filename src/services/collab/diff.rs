1fd851632b750ea86bb16ffc390b33007c2ea4ee
Susa Milán <mailtosnek@protonmail.com>
2023-02-22 10:28:18 +0100

    Added diff api based on commits

--- src/services/collab/diff.rs
+++ src/services/collab/diff.rs
@@ -1,13 +1,63 @@
-use actix_web::{get, web::Query, Responder};
+use actix_web::{
+    get,
+    web::{Path, Query},
+    HttpResponse, Responder,
+};
+use git2::{DiffOptions, Oid, Repository};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, PartialEq)]
-enum Show {
-    BranchToBranch,
-    CommitToCommit,
+struct Qy {
+    opts: Show,
 }
 
-#[get("/show")]
-async fn show(opts: Query<Show>) -> impl Responder {
-    "shown!"
+#[derive(Deserialize, Serialize, PartialEq)]
+enum Show {
+    #[serde(rename = "branch2branch")]
+    BranchToBranch(FT),
+    #[serde(rename = "commit2commit")]
+    CommitToCommit(FT),
+}
+
+#[derive(Deserialize, Serialize, PartialEq)]
+pub struct FT {
+    from: String,
+    to: String,
+}
+
+#[get("/diff/show/{user_dir}/{repo_dir}")]
+pub async fn show(path: Path<(String, String)>, opts: Query<FT>) -> impl Responder {
+    info!("asd");
+    let repo = Repository::open(format!("git_test/{}/{}", path.0, path.1)).unwrap();
+    let opts = opts.into_inner();
+
+    let old_commit = repo
+        .find_commit(Oid::from_str(&opts.from).unwrap())
+        .unwrap();
+    let new_commit = repo.find_commit(Oid::from_str(&opts.to).unwrap()).unwrap();
+
+    let diff = repo
+        .diff_tree_to_tree(
+            Some(&old_commit.tree().unwrap()),
+            Some(&new_commit.tree().unwrap()),
+            None,
+        )
+        .unwrap();
+
+    let mut diff_opts = DiffOptions::new();
+    diff_opts.ignore_whitespace(false);
+
+    let mut buffer = Vec::new();
+    diff.print(git2::DiffFormat::Patch, |_delta, _hunk, line| {
+        buffer.extend_from_slice(line.content());
+        true
+    })
+    .unwrap();
+
+    HttpResponse::Ok().body(buffer)
+}
+
+#[get("/test")]
+pub async fn asd() -> impl Responder {
+    "asd"
 }
